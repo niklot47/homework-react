64a9457a055c18fcb509471f22beb9bcc623f0e7
MykolaKozakov <niklot47@gmail.com>
2022-01-17 22:00:43 +0200

    CW01

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
+  let [flightsList, setFlightsList] = useState([]);
+  useEffect(()=>{
+    fetch('https://api.spacexdata.com/v3/launches/')
+        .then(value => value.json())
+        .then(flights => {
+          setFlightsList(flights.filter(flight => flight.launch_year !== '2020'));
+        })
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        flightsList.map(value => <div className='flight' key={value.flight_number}>
+          {value.mission_name} ({value.launch_year})
+          <img src={value.links.mission_patch} alt='mission_patch'/>
+        </div>)
+      }
     </div>
   );
 }
