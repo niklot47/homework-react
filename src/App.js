00734fad0df37abc3b21b43e095debc01ae5df52
MykolaKozakov <niklot47@gmail.com>
2022-01-22 16:58:51 +0200

    hw-03

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React from "react";
 
-import Users from "./components/Users/Users";
-import UserDetails from "./components/UserDetails/UserDetails";
-import Posts from "./components/Posts/Posts";
+import css from './App.module.css'
+import {NavLink, Routes, Route} from "react-router-dom";
+import UsersPage from "./pages/UsersPage/UsersPage";
+import PostsPage from "./pages/PostsPage/PostsPage";
+import Layout from "./components/Layout/Layout";
+import NotFountPage from "./pages/NotFoundPage/NotFountPage";
 
 function App() {
     return (
-        <div className="App">
-            <div className='header'>
-                <Users/>
-                <UserDetails/>
-            </div>
-            <Posts/>
-        </div>
+        <>
+            <Routes>
+                <Route path={'/'} element={<Layout/>}>
+                    <Route path={'/'} element={<UsersPage/>}/>
+                    <Route path={'/posts'} element={<PostsPage/>}/>
+                    <Route path={'*'} element={<NotFountPage/>}/>
+                </Route>
+            </Routes>
+        </>
     );
 }
 
